7a48ca26e3ffbe2f67bb921fc3e610b2c7aaf8dd
Avelar19 <thiago.avelar19@gmail.com>
2023-11-12 16:37:09 -0300

    Atualizacao db 1.0

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@ let mysql = require('mysql2/promise');
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'root123',
+    password: 'root',
     database: 'projetoapi'
 });
 
